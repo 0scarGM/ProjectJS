b2d8dc31ee59959ca1fdc29f69492f55ea9c0779
unknown <osccarm03@gmail.com>
2024-06-17 01:58:03 -0600

    temas

--- usuarios.js
+++ usuarios.js
@@ -7,13 +7,64 @@ let bregistro = document.getElementById("Registrarse")
 
 let usuario = []
 
+let tema=document.getElementById("tema")
+let tema1=document.getElementById("tema1")
+let tema2=document.getElementById("tema2")
+let tema3=document.getElementById("tema3")
+let body=document.getElementById("body")
+
+tema.addEventListener("click",()=>{
+    body.classList.add("body")
+    body.classList.remove("body1")
+    body.classList.remove("body2")
+    body.classList.remove("body3")
+    localStorage.setItem("body", 0)
+})
+tema1.addEventListener("click",()=>{
+    body.classList.remove("body")
+    body.classList.add("body1")
+    body.classList.remove("body2")
+    body.classList.remove("body3")
+    localStorage.setItem("body", 1)
+})
+tema2.addEventListener("click",()=>{
+    body.classList.remove("body")
+    body.classList.remove("body1")
+    body.classList.add("body2")
+    body.classList.remove("body3")
+    localStorage.setItem("body", 2)
+})
+tema3.addEventListener("click",()=>{
+    body.classList.remove("body")
+    body.classList.remove("body1")
+    body.classList.remove("body2")
+    body.classList.add("body3")
+    localStorage.setItem("body", 3)
+})
+
 document.addEventListener("DOMContentLoaded", carga)
 
+
 function carga(){
+    
     if (localStorage.getItem("usuario") === null) {
         usuario = []
     } else {
         usuario = JSON.parse(localStorage.getItem("usuario"))
+    }
+    switch(localStorage.getItem("body")){
+        case "1":
+            body.classList.add("body1")
+        break
+        case "2":
+            body.classList.add("body2")
+        break
+        case "3":
+            body.classList.add("body3")
+        break
+        default:
+            body.classList.add("body")
+        break
     }
 }
 
@@ -27,7 +78,7 @@ bregistro.addEventListener("click", () =>{
 
     localStorage.setItem("usuario", JSON.stringify(usuario))
     
-    cargar()
+    carga()
 })
 
 biniciar.addEventListener("click", () =>{
